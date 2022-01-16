2968e9e5891798d8632d85321524f6ce6943161c
ghoulamedz <91066440+ghoulamedz@users.noreply.github.com>
2022-01-16 17:29:29 +0100

    simple stack impl

--- src/Person.java
+++ src/Person.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Person {
-    ArrayList<String> person = new ArrayList<String>();
-    String[] persons = {"jhon","mohamed","sam","jeff","ranim","mejdi","saddam","imed","klay","michael"};
-    ArrayList<String> personsList = new ArrayList<String>(Arrays.asList(persons));
-    public static Person oldestFriend(Stream<Person> stream){
-        return stream.flatMap(mapper)
-    }
-  }
+//    ArrayList<String> person = new ArrayList<String>();
+//    String[] persons = {"jhon","mohamed","sam","jeff","ranim","mejdi","saddam","imed","klay","michael"};
+//    ArrayList<String> personsList = new ArrayList<String>(Arrays.asList(persons));
+//    public static Person oldestFriend(Stream<Person> stream){
+//        return stream.flatMap(mapper);
+//    }
+}
